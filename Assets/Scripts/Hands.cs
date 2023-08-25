3d20c37a6181a357b48003933cc982035083729c
colorblvnd <140036573+colorblvnd@users.noreply.github.com>
2023-08-24 20:10:32 -0400

    Initial Commit
    - Initial commit

---
+++ Assets/Scripts/Hands.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Hands : XRDirectInteractor
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
